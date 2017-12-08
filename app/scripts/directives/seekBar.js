da0822c67c5af2102c8ac58dd3f9310efe5c2f71
Noel Arroyo <noel.arroyo1@gmail.com>
2017-12-08 10:04:02 -0600

    add some changes to thumb

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -28,7 +28,11 @@
               };
 
               scope.fillStyle = function(){
-                return {width: percentString()};
+                return{width: percentString()}
+              };
+
+              scope.thumbStyle = function(){
+                return {left: percentString()};
               };
 
               scope.onClickSeekBar = function(event) {
